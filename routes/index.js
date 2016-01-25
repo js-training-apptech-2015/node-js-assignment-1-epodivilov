a4b35ac0f82ea8d567a76001eb75e41b68904c04
Подивилов Евгений <evgeniy.podivilov@gmail.com>
2016-01-25 14:14:53 +0500

    Implemented GET method for /games/token  Implemented POST method for create new game. Selecting a server to create a new play performed by the algorithm Round-robin

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var request = require('request');
-
 var externalServers = require('../config').servers;
+var currentServer = externalServers;
 
 module.exports = function (server) {
     server.get('/games', function (req, res, next) {
@@ -22,5 +22,44 @@ module.exports = function (server) {
         Promise.all(promises).then(function (values) {
             res.send(values);
         })
-    })
+    });
+
+    server.get('/games/:id', function (req, res, next) {
+        var promises = [];
+
+        externalServers.forEach(function (url) {
+            promises.push(new Promise(function (resolve, reject) {
+                request(url + 'games/' + req.params.id, function (error, response, body) {
+                    if (error) reject(error);
+
+                    resolve({
+                        "server": url.match('\/\/([a-z0-9-]+)\.')[1],
+                        "data": JSON.parse(body)
+                    });
+                });
+            }));
+        });
+
+        Promise.all(promises).then(function (values) {
+            res.send(values);
+        })
+    });
+
+    server.post('/games', function (req, res, next) {
+        var url = currentServer.shift();
+        currentServer.push(url);
+
+        request({
+            uri: url + 'games',
+            method: 'POST',
+            json: true,
+            body: {type:0}
+        }, function (error, response, body) {
+            if (error) return (error);
+            res.send({
+                "server": url.match('\/\/([a-z0-9-]+)\.')[1],
+                "data": body
+            });
+        });
+    });
 };
